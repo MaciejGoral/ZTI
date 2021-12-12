5a12b50ae98e0b1a4cacfff95b15d45d3c6acc2b
Maciej Goral <maciej636107@gmail.com>
2021-12-12 17:19:02 +0100

    MLPClassifier

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from rdflib import Graph,Literal, RDF,URIRef
 from rdflib.namespace import FOAF, XSD
+from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import pandas as pd
@@ -65,19 +68,64 @@ def test_file():
 
 df_train=pd.read_csv("train.csv")
 df_test=pd.read_csv("test.csv")
-le = LabelEncoder()
-le.fit(np.unique(df_train.astype(str)))
-X = np.array([le.transform(samp.astype(str)) for samp in df_train.values])
-print(X)
-df1=pd.DataFrame(X,columns=["id","score","predicate","subject","object"])
-df1 = df1.iloc[: , 1:]
-df1.to_csv("train1.csv")
 
+# df_tr_sc=df_train['score']
+# df_tr_p=df_train['predicate']
+# df_tr_sub=df_train['subject']
+# df_tr_ob=df_train['object']
 
-# X_train=df_train.drop('score',axis=1)
-# X_test=df_test
-# Y_train=df_train['score']
-#
-# clf =RandomForestClassifier(random_state=0)
-# clf.fit(X_train,Y_train)
-# clf.predict(X_test)
+# df_train_1=df_train.drop('score',axis=1)
+# df_test_1=df_test.rename(columns={"subject": "sub", "predicate": "pred","object":"obj"})
+# result = pd.concat([df_train_1, df_test_1], axis=1, join='inner')
+# result=result.drop('Unnamed: 0',axis=1)
+# result.to_csv("result.csv")
+# le = LabelEncoder()
+# le.fit(np.unique(result.astype(str)))
+# X = np.array([le.transform(samp.astype(str)) for samp in result.values])
+# print(X)
+# df1=pd.DataFrame(X,columns=["predicate","subject","object","sub","pred","obj"])
+# df1.to_csv("result.csv")
+df_fin=pd.read_csv("result.csv")
+df1 = df_fin[['predicate', 'subject','object']]
+df2 = df_fin[['sub', 'pred','obj']]
+
+df_pom1=df2['sub']
+df_pom2=df2['pred']
+df_pom3=df2['obj']
+df_pom4=pd.concat([df_pom2, df_pom1], axis=1, join='inner')
+df2=pd.concat([df_pom4, df_pom3], axis=1, join='inner')
+
+df3=pd.concat([df1, df_train["score"]], axis=1, join='inner')
+df4=df2.rename(columns={"pred": "predicate","sub": "subject","obj":"object"})
+df4.to_csv("test_en.csv")
+df3.to_csv("train_en.csv")
+
+X_train=df1
+X_test=df4
+Y_train=df3['score']
+
+clf =RandomForestClassifier(random_state=0)
+clf.fit(X_train,Y_train)
+pred=clf.predict(X_test)
+res=pd.DataFrame(pred)
+
+print(classification_report(Y_train,pred))
+
+# clf2=svm.SVC()
+# clf2.fit(X_train,Y_train)
+# pred2=clf2.predict(X_test)
+# print(pred2)
+# print(classification_report(Y_train,pred2))
+tab=[]
+for i in range(100):
+    mlpc=MLPClassifier(hidden_layer_sizes=(11,11,11),max_iter=500)
+    mlpc.fit(X_train,Y_train)
+    pred_mlpc=mlpc.predict(X_test)
+    tab.append(pred_mlpc)
+    print(pred_mlpc)
+    print(classification_report(Y_train,pred_mlpc))
+
+df_pom_10=pd.DataFrame(tab)
+pom=df_pom_10.mean()
+print(pom)
+pom.to_csv("final_res.csv")
